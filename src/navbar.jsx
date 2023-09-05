19f03777dff6835633cb5db76bc63d466de45909
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-09-05 13:57:30 -0600

    new update NavBar

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,45 +1,34 @@
-import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
-function ColorSchemesExample() {
+function BasicExample() {
 	return (
-		<>
-			<Navbar bg="dark" variant="dark">
-				<Container>
-					<Navbar.Brand href="#home">Navbar (Dark)</Navbar.Brand>
+		<Navbar expand="lg" className="bg-body-tertiary">
+			<Container>
+				<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="me-auto">
 						<Nav.Link href="#home">Home</Nav.Link>
-						<Nav.Link href="#features">Features</Nav.Link>
-						<Nav.Link href="#pricing">Pricing</Nav.Link>
+						<Nav.Link href="#link">Link</Nav.Link>
+						<NavDropdown title="Dropdown" id="basic-nav-dropdown">
+							<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.2">
+								Another action
+							</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+							<NavDropdown.Divider />
+							<NavDropdown.Item href="#action/3.4">
+								Separated link
+							</NavDropdown.Item>
+						</NavDropdown>
 					</Nav>
-				</Container>
-			</Navbar>
-			<br />
-			<Navbar bg="primary" variant="dark">
-				<Container>
-					<Navbar.Brand href="#home">Navbar (Primary)</Navbar.Brand>
-					<Nav className="me-auto">
-						<Nav.Link href="#home">Home</Nav.Link>
-						<Nav.Link href="#features">Features</Nav.Link>
-						<Nav.Link href="#pricing">Pricing</Nav.Link>
-					</Nav>
-				</Container>
-			</Navbar>
-			<br />
-			<Navbar bg="light" variant="light">
-				<Container>
-					<Navbar.Brand href="#home">Navbar (Light)</Navbar.Brand>
-					<Nav className="me-auto">
-						<Nav.Link href="#home">Home</Nav.Link>
-						<Nav.Link href="#features">Features</Nav.Link>
-						<Nav.Link href="#pricing">Pricing</Nav.Link>
-					</Nav>
-				</Container>
-			</Navbar>
-		</>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
 	);
 }
 
-export default ColorSchemesExample;
+export default BasicExample;
