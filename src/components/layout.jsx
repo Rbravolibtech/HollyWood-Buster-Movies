80ad527fac1c468483d3308e86c7888d0a189cc9
whittlialleman <alle.alleman@gmail.com>
2023-09-06 13:43:54 -0600

    Altered styling a bit

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,18 +1,22 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import '../index.css'
 
 export default function Layout(props) {
 	return (
 		<div className="navbar">
 			<header>
-				<Navbar expand="lg" className="bg-body-tertiary">
+				<Navbar expand="lg" className="bg-body-tertiary sideNav" style={{ width: '10%', paddingTop: '5%' }}>
 					<Container>
 						<Navbar.Collapse id="basic-navbar-nav">
 							<Nav className="me-auto">
 								<Nav.Link href="/">Home</Nav.Link>
+								<br />
 								<Nav.Link href="movies">Movies</Nav.Link>
+								<br />
 								<Nav.Link href="tvseries">TvSeries</Nav.Link>
+								<br />
 								<Nav.Link href="search">Search</Nav.Link>
 							</Nav>
 						</Navbar.Collapse>
@@ -23,6 +27,6 @@ export default function Layout(props) {
 			<footer>
 				<p>Powered by TMDb</p>
 			</footer>
-		</div>
+		</div >
 	);
 }
