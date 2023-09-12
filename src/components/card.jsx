665df92bbb797b2836a9639801514e98b9a4510a
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-09-12 16:06:12 -0600

    updated file for card jsx

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
 
 function Cards({ movie }) {
 	return (
-		<Card className="bg-dark text-white">
-			<Card.Img src={movie.image} />
-			<Card.ImgOverlay>
-				<Card.Title>
-					{movie.title} | {movie.date}
-				</Card.Title>
-			</Card.ImgOverlay>
-		</Card>
+		<div>
+			<img src={movie.image} />
+			<h4>
+				{movie.title} | {movie.date}
+			</h4>
+		</div>
 	);
 }
 export default Cards;
