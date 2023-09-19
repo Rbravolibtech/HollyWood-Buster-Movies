c1406d9950100cdd9b18c9caafac34716cb6906a
whittlialleman <alle.alleman@gmail.com>
2023-09-19 15:51:59 -0600

    Started some title page styling

--- src/pages/title.jsx
+++ src/pages/title.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styles from "./index.module.css";
 import { useSearchParams } from "react-router-dom";
 import { getTitlePageInfo } from "../lib/API/Api";
 
@@ -37,19 +36,21 @@ function Page() {
 
 	if (error !== "") return <p>ERROR BUDDY</p>
 
+	const type = searchParams.get("type");
+
 	return (
 		<div className="titleMain">
-			<div>
-				<img src={movie.photo} alt="" />
-			</div>
-			<div>
+			<div className="titlePhoto" style={{ backgroundImage: `linear-gradient(90deg, rgba(161, 0, 255, 0) 20%, rgba(0, 0, 0, 0) 84%, rgba(0, 0, 0, 0) 96%), url(${movie.photo})` }}>
 				<h1>{movie.title}</h1>
+				<p>{movie.overview}</p>
 			</div>
-
-			<div>
-				<p>Rating: {movie.rating}</p>
-				<p>Release Year: {movie.date}</p>
-				<p>{movie.type === 'movie' ? `${movie.runtime} Minutes` : `${movie.runtime} Seasons`}</p>
+			<div className="titleInfo">
+				<img src={movie.poster} alt="" />
+				<div>
+					<p>Rating: {movie.rating}</p>
+					<p>Release Year: {movie.date}</p>
+					<p>{type === 'movie' ? `Runtime: ${movie.runtime} Minutes` : `Seasons: ${movie.runtime}`}</p>
+				</div>
 			</div>
 		</div>
 	);
