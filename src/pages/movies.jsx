e267558f3f6f59787330c94bd0fcc7b3bd781153
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-09-27 13:13:24 -0600

    added code for movies and tvseries

--- src/pages/movies.jsx
+++ src/pages/movies.jsx
@@ -1,5 +1,49 @@
+import React, { useEffect, useState } from "react";
+import styles from "./index.module.css";
+import Card from "../components/card";
+import { getTrendingMovies } from "../lib/API/Api";
+
 function Page() {
-	return <p>Movies</p>;
+	const [loadingMovies, setLoadingMovies] = useState(false);
+	const [errorMovies, setErrorMovies] = useState("");
+	const [movies, setMovies] = useState([]);
+
+	useEffect(() => {
+		setLoadingMovies(true);
+		const fetchData = async () => {
+			try {
+				const moviesArray = await getTrendingMovies();
+				if (moviesArray.length < 1) {
+					setErrorMovies("No movies found");
+				} else {
+					setMovies(moviesArray);
+				}
+			} catch (error) {
+				setErrorMovies("An error occurred while fetching movies.");
+			} finally {
+				setLoadingMovies(false);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	return (
+		<div>
+			<div className={styles.titleCards}>
+				<h2>Movies</h2>
+				<div className={styles.movies}>
+					{loadingMovies && <p>Loading</p>}
+					{errorMovies && <p>{errorMovies}</p>}
+					{!loadingMovies &&
+						!errorMovies &&
+						movies.map((movie) => {
+							return <Card key={movie.id} movie={movie} />;
+						})}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Page;
--- src/pages/tvseries.jsx
+++ src/pages/tvseries.jsx
@@ -1,6 +1,49 @@
+import React, { useState, useEffect } from "react";
+import styles from "./index.module.css";
+import Card from "../components/card";
+import { getTrendingTVShows } from "../lib/API/Api";
+
 function Page() {
-	return <p>Tv Series</p>;
+	const [loadingTVShows, setLoadingTVShows] = useState(false);
+	const [errorTVShows, setErrorTVShows] = useState("");
+	const [TVShows, setTVShows] = useState([]);
+
+	useEffect(() => {
+		setLoadingTVShows(true);
+		const tvFetch = async () => {
+			try {
+				const tvArray = await getTrendingTVShows();
+				if (tvArray.length < 1) {
+					setErrorTVShows("No TV shows found");
+				} else {
+					setTVShows(tvArray);
+				}
+			} catch (error) {
+				setErrorTVShows("An error occurred while fetching TV shows.");
+			} finally {
+				setLoadingTVShows(false);
+			}
+		};
+
+		tvFetch();
+	}, []);
+
+	return (
+		<div>
+			<div className={styles.titleCards}>
+				<h2>TV Shows</h2>
+				<div className={styles.movies}>
+					{loadingTVShows && <p>Loading</p>}
+					{errorTVShows && <p>{errorTVShows}</p>}
+					{!loadingTVShows &&
+						!errorTVShows &&
+						TVShows.map((tvShow) => {
+							return <Card key={tvShow.id} movie={tvShow} />;
+						})}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Page;
-
