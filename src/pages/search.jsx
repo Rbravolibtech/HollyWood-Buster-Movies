c2b2808fa466c3cc4103a06538e2e88e734be443
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-09-27 15:51:09 -0600

    search updated with some new code

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,21 +1,21 @@
-// import { getTrendingMovies, getTrendingTVShows } from "../lib/API/Api";
+import { getTrendingMovies, getTrendingTVShows } from "../lib/API/Api";
 
-// const Search = ({ keyword, onChange }) => {
-// 	const BarStyle = {
-// 		width: "20rem",
-// 		background: "#F0F0F0",
-// 		border: "none",
-// 		padding: "0.5rem",
-// 	};
-// 	return (
-// 		<input
-// 			style={BarStyle}
-// 			key="search-bar"
-// 			value={keyword}
-// 			placeholder={"search"}
-// 			onChange={(e) => onChange(e.target.value)}
-// 		/>
-// 	);
-// };
+const Search = ({ keyword, onChange }) => {
+	const BarStyle = {
+		width: "20rem",
+		background: "#F0F0F0",
+		border: "none",
+		padding: "0.5rem",
+	};
+	return (
+		<input
+			style={BarStyle}
+			key="search-bar"
+			value={keyword}
+			placeholder={"search"}
+			onChange={(e) => onChange(e.target.value)}
+		/>
+	);
+};
 
-// export default Search;
+export default Search;
