296183f3011bc0e9258a6f552953c5bcc8d2f1de
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-09-29 14:03:35 -0600

    added code  to search file

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -2,22 +2,35 @@ import React, { useState, useEffect } from "react";
 import { getSearchResults } from "../lib/API/Api";
 import Card from "../components/card";
 
-const Search = ({ keyword, onChange }) => {
+function Page() {
+	const [searchText, setSearchText] = useState("");
+	const [page, setPage] = useState(1);
+	const [searchResults, setSearchResults] = useState([]);
+
 	const BarStyle = {
 		width: "100%",
 		background: "#F0F0F0",
 		border: "2px solid #ccc",
 		padding: "0.8% ",
 	};
-	return (
-		<input
-			style={BarStyle}
-			key="search-bar"
-			value={keyword}
-			placeholder={"SEARCH MOVIES AND TV SHOWS"}
-			onChange={(e) => onChange(e.target.value)}
-		/>
-	);
-};
 
-export default Search;
+	const submitQuery = async (e) => {
+		e.preventDefault();
+		const result = await getSearchResults(searchText);
+		setSearchResults(result);
+	};
+
+	return (
+		<form onSubmit={submitQuery}>
+			<input
+				style={BarStyle}
+				value={searchText}
+				placeholder={"SEARCH MOVIES AND TV SHOWS"}
+				onInput={(e) => setSearchText(e.target.value)}
+			/>
+			<input type="submit" />
+		</form>
+	);
+}
+
+export default Page;
