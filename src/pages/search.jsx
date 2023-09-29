c3965774f54f2b2c196d27e7b7469abd9270f5e6
whittlialleman <alle.alleman@gmail.com>
2023-09-29 15:10:58 -0600

    Made some changes to the search page structure

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -5,8 +5,10 @@ import styles from "./search.module.css";
 
 function Page() {
 	const [searchText, setSearchText] = useState("");
-	const [page, setPage] = useState(1);
+	// const [page, setPage] = useState(1);
 	const [searchResults, setSearchResults] = useState([]);
+	const [loadingSearchResults, setLoadingSearchResults] = useState(false);
+	const [errorResults, setErrorResults] = useState("");
 
 	const submitQuery = async (e) => {
 		e.preventDefault();
@@ -14,16 +16,50 @@ function Page() {
 		setSearchResults(result);
 	};
 
+	useEffect(() => {
+		setLoadingSearchResults(true);
+		const fetchData = async () => {
+			try {
+				const searchArray = await getSearchResults(searchText);
+				if (searchArray.length < 1) {
+					setErrorResults("No results found");
+				} else {
+					setSearchResults(searchArray);
+				}
+			} catch (error) {
+				setErrorResults("An error occurred while fetching results.");
+			} finally {
+				setLoadingSearchResults(false);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+
 	return (
-		<form onSubmit={submitQuery}>
-			<input
-				className={styles.barStyle}
-				value={searchText}
-				placeholder={"SEARCH MOVIES AND TV SHOWS"}
-				onInput={(e) => setSearchText(e.target.value)}
-			/>
-			<input type="submit" />
-		</form>
+		<>
+			<form onSubmit={submitQuery}>
+				<input
+					className={styles.barStyle}
+					value={searchText}
+					placeholder={"SEARCH MOVIES AND TV SHOWS"}
+					onInput={(e) => setSearchText(e.target.value)}
+				/>
+				<input type="submit" />
+			</form>
+			<div className={styles.titleCards}>
+				<div className={styles.movies}>
+					{loadingSearchResults && <p>Loading</p>}
+					{errorResults && <p>{errorResults}</p>}
+					{!loadingSearchResults &&
+						!errorResults &&
+						searchResults.map((movie) => {
+							return <Card key={movie.id} movie={movie} />;
+						})}
+				</div>
+			</div>
+		</>
 	);
 }
 
